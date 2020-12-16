e7905947f5fb4b777942359fa25e62025fbf41a0
alpi-killa <alperenasan@gmail.com>
2020-12-16 22:35:53 +0300

    github'a push etme denemesi

--- app/src/main/java/com/egeuzma/proje/Controller/KaloriHesaplayici.kt
+++ app/src/main/java/com/egeuzma/proje/Controller/KaloriHesaplayici.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import com.egeuzma.proje.R
 
 class KaloriHesaplayici : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kalori_hesaplayici)
     }
+
 }
